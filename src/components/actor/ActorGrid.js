c89fc0d017842106c408b43b358da41197f59945
Mehakpreet Kaur <mehakpreetk579@gmail.com>
2022-07-22 00:37:59 +0530

    commit

---
+++ src/components/actor/ActorGrid.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ActorCard from './ActorCard';
+
+// import image
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
+
+const ActorGrid = ({ data }) => {
+  return (
+    <FlexGrid>
+      {data.map(({ person }) => (
+        <ActorCard
+          key={person.id}
+          id={person.id}
+          name={person.name}
+          country={person.country ? person.country.name : null}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          gender={person.gender}
+          image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+        />
+      ))}
+    </FlexGrid>
+  );
+};
+
+export default ActorGrid;
